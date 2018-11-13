a8ed6437049eb25a69a36323a71884438cd360e6
Youhan Cheery <youhancheery@Youhans-MacBook-Pro.local>
2018-11-13 23:07:27 +1100

    created title column, started age imputing

--- src/analysis.R
+++ src/analysis.R
@@ -1,40 +1,51 @@
 # predict if a passanger would survive the titanic
 # output should be to gender_submission.csv
 library(tidyverse)
+setwd("~/Documents/kaggle/titanic")
 
 # read the train/test data in
+# TODO: fix path by setting working directory properly
 train <- read.csv('data/train.csv')
 test <- read.csv('data/test.csv')
 
-# bind rows
-# ensure that no scaling occurs at this point
-# need to prevent train/test leakage
-
-
-# NOTE
-# THIS IS NOT GOOD PRACTICE AND SHOULD BE DONE WITH CAUTION
-# IDEALLY THE AUGMENTATION APPLIED TO TRAIN SHOULD BE REPEATED TO TEST SEPERATELY
-# TRAIN AND TEST SHOULD NOT BE AWARE OF EACH OTHERS' EXISTANCE
-# LEAKAGE MAY CAUSE OVERFITTING ESP. SINCE WE DON'T HAVE A CV SET HERE
-data <- dplyr::bind_rows(train, test) # bind rows, ignoring differences in cols
-
 ##### EXPLORATORY DATA ANALYSIS #####
 # view the data
-head(data)
-
+head(train)
+head(test)
 # convert categorical 'Sex' variable to binary
-unique(data$Sex)
+unique(train$Sex)
 # TODO: a better way of doing this
-data[,"Sex"] <- sapply(data[,"Sex"],switch,"male"=0,"female"=1)
+train[,"Sex"] <- sapply(train[,"Sex"],switch,"male"=0,"female"=1)
 
 # how many unique values do we have in each column?
-lapply(data, function(x) length(unique(x)))
+lapply(train, function(x) length(unique(x)))
 
 # what are the unique values in columns not including PassengerId
-lapply(data[-which(names(data) %in% c("PassengerId", "Name", "Ticket",
+lapply(train[-which(names(train) %in% c("PassengerId", "Name", "Ticket",
                                               "Age", "Fare", "Cabin"))], 
              function(x) unique(x))
 
 # count NAs in each column
-lapply(data, function(x) sum(is.na(x)))
+lapply(train, function(x) sum(is.na(x)))
 
+# see that Age has a lot of NAs
+# however age is too important a variable to just drop
+# TODO: find a way to fill missing NAs
+sprintf("%2f", 100.0*sum(is.na(train$Age))/nrow(train))
+
+# create title variable that gets the title from full name
+train$Title <- as.factor(gsub("(.+), ([A-Za-z]+\\.) (.+)", "\\2", train$Name, perl=TRUE))
+print(unique(train$Title))
+
+# impute the age column using the following rule
+# calculate the average/median for the different titles+children combos
+# insert the average/median into the NA values for age
+
+
+
+# one hot encode Embarked column
+
+# in model file
+# try decision tree
+# try xgboost
+# try random forest
